db84e44a481ed925a0167a087d7344851b8afdc6
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-07 00:44:28 +0300

    Add unit tests for validation and exceptions

---
+++ lab4/src/test/java/com/example/ClothesTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClothesTest {
+
+    @Test
+    void testInvalidConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Clothes("", "summer", -10, "M");
+        });
+    }
+
+    @Test
+    void testInvalidSetter() {
+        Clothes c = new Clothes("shirt", "summer", 100, "M");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            c.setPrice(-5);
+        });
+    }
+}
