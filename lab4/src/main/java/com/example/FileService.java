b92ec1ad88aaf9cdd44dd82f6a518c1d6cc09c70
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-07 19:14:59 +0300

    Add FileService with load and save functionality

---
+++ lab4/src/main/java/com/example/FileService.java
@@ -0,0 +1,102 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileService {
+
+    public static ArrayList<Clothes> loadFromFile(String fileName) {
+
+        ArrayList<Clothes> list = new ArrayList<>();
+
+        try (Scanner sc = new Scanner(new File(fileName))) {
+
+            while (sc.hasNextLine()) {
+
+                String[] parts = sc.nextLine().split(";");
+                String type = parts[0];
+
+                switch (type) {
+
+                    case "CLOTHES":
+                        list.add(new Clothes(parts[1],
+                                ClothesType.valueOf(parts[2]),
+                                Double.parseDouble(parts[3]),
+                                parts[4]));
+                        break;
+
+                    case "PANTS":
+                        list.add(new Pants(parts[1],
+                                ClothesType.valueOf(parts[2]),
+                                Double.parseDouble(parts[3]),
+                                parts[4],
+                                parts[5]));
+                        break;
+
+                    case "SHIRTS":
+                        list.add(new Shirts(parts[1],
+                                ClothesType.valueOf(parts[2]),
+                                Double.parseDouble(parts[3]),
+                                parts[4],
+                                Boolean.parseBoolean(parts[5])));
+                        break;
+
+                    case "JACKET":
+                        list.add(new Jacket(parts[1],
+                                ClothesType.valueOf(parts[2]),
+                                Double.parseDouble(parts[3]),
+                                parts[4],
+                                Boolean.parseBoolean(parts[5])));
+                        break;
+
+                    case "SHOES":
+                        list.add(new Shoes(parts[1],
+                                ClothesType.valueOf(parts[2]),
+                                Double.parseDouble(parts[3]),
+                                parts[4],
+                                parts[5]));
+                        break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+
+        return list;
+    }
+
+    public static void saveToFile(ArrayList<Clothes> list, String fileName) {
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+
+            for (Clothes c : list) {
+
+                if (c instanceof Pants p) {
+                    writer.write("PANTS;" + p.getName() + ";" + p.getType() + ";" + p.getPrice() + ";" + p.getSize() + ";" + p.getMaterial() + "\n");
+                }
+
+                else if (c instanceof Shirts s) {
+                    writer.write("SHIRTS;" + s.getName() + ";" + s.getType() + ";" + s.getPrice() + ";" + s.getSize() + ";" + s.isLongSleeve() + "\n");
+                }
+
+                else if (c instanceof Jacket j) {
+                    writer.write("JACKET;" + j.getName() + ";" + j.getType() + ";" + j.getPrice() + ";" + j.getSize() + ";" + j.isHood() + "\n");
+                }
+
+                else if (c instanceof Shoes sh) {
+                    writer.write("SHOES;" + sh.getName() + ";" + sh.getType() + ";" + sh.getPrice() + ";" + sh.getSize() + ";" + sh.getSoleType() + "\n");
+                }
+
+                else {
+                    writer.write("CLOTHES;" + c.getName() + ";" + c.getType() + ";" + c.getPrice() + ";" + c.getSize() + "\n");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+    }
+}
