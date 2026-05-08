2f373fa566dad0ffa3ef1aee0348dabc4ad3dd8b
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-08 15:39:46 +0300

    Finish lab16

--- lab4/src/main/java/com/example/MainApp.java
+++ lab4/src/main/java/com/example/MainApp.java
@@ -9,8 +9,10 @@ import javafx.stage.Stage;
 public class MainApp extends Application {
 
     private Store store = new Store();
+    
     private TextField nameField = new TextField();
     private ComboBox<String> typeBox = new ComboBox<>();
+    private ComboBox<String> categoryBox = new ComboBox<>();
 
     private TextField priceField = new TextField();
     private TextField sizeField = new TextField();
@@ -24,7 +26,8 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        typeBox.getItems().addAll("PANTS", "SHIRTS", "JACKET", "SHOES");
+        categoryBox.getItems().addAll("PANTS", "SHIRTS", "JACKET", "SHOES");
+        typeBox.getItems().addAll("SUMMER", "WINTER", "AUTUMN", "SPRING");
 
         Button addBtn = new Button("Add");
         Button searchBtn = new Button("Search UUID");
@@ -34,44 +37,59 @@ public class MainApp extends Application {
         // ДОДАТИ ОБ'ЄКТ
         addBtn.setOnAction(e -> {
 
-            String name = nameField.getText();
-            String type = typeBox.getValue();
-            String size = sizeField.getText();
-            double price = 0;
+            if (nameField.getText().isEmpty()
+                    || priceField.getText().isEmpty()
+                    || sizeField.getText().isEmpty()
+                    || categoryBox.getValue() == null
+                    || typeBox.getValue() == null) {
+
+                output.appendText("Fill all required fields!\n");
+                return;
+            }
+
+            double price;
             try {
                 price = Double.parseDouble(priceField.getText());
-            } catch (Exception ex) {
-                output.appendText("Invalid price!\n");
+                if (price <= 0) {
+                    output.appendText("Price must be > 0\n");
+                    return;
+                }
+            } catch (NumberFormatException ex) {
+                output.appendText("Invalid price format!\n");
                 return;
-            }            
+            }
+
+            String name = nameField.getText();
+            String size = sizeField.getText();
+
+            ClothesType type = ClothesType.valueOf(typeBox.getValue());
 
             Clothes c;
 
-            switch (type) {
+            switch (categoryBox.getValue()) {
 
-                case "PANTS":
-                    String material = materialField.getText();
-                    c = new Pants(name, ClothesType.valueOf(type), price, size, material);
-                    break;
+                case "PANTS" -> {
+                    c = new Pants(name, type, price, size, materialField.getText());
+                }
 
-                case "SHIRTS":
+                case "SHIRTS" -> {
                     boolean longSleeve = Boolean.parseBoolean(longSleeveField.getText());
-                    c = new Shirts(name, ClothesType.valueOf(type), price, size, longSleeve);
-                    break;
+                    c = new Shirts(name, type, price, size, longSleeve);
+                }
 
-                case "JACKET":
+                case "JACKET" -> {
                     boolean hood = Boolean.parseBoolean(hoodField.getText());
-                    c = new Jacket(name, ClothesType.valueOf(type), price, size, hood);
-                    break;
+                    c = new Jacket(name, type, price, size, hood);
+                }
 
-                case "SHOES":
-                    String soleType = soleTypeField.getText();
-                    c = new Shoes(name, ClothesType.valueOf(type), price, size, soleType);
-                    break;
+                case "SHOES" -> {
+                    c = new Shoes(name, type, price, size, soleTypeField.getText());
+                }
 
-                default:
-                    output.appendText("Unknown type!\n");
+                default -> {
+                    output.appendText("Unknown category!\n");
                     return;
+                }
             }
 
             store.addNewClothes(c, 1);
@@ -91,18 +109,21 @@ public class MainApp extends Application {
 
         VBox root = new VBox(
                 new Label("Name"), nameField,
-                new Label("Type"), typeBox,
+
+                new Label("Category (PANTS/SHIRTS/JACKET/SHOES)"), categoryBox,
+                new Label("Season (ClothesType)"), typeBox,
+
                 new Label("Price"), priceField,
                 new Label("Size"), sizeField,
 
                 new Label("Material (PANTS)"), materialField,
-                new Label("Long Sleeve (SHIRTS)"), longSleeveField,
-                new Label("Hood (JACKET)"), hoodField,
+                new Label("Long Sleeve (SHIRTS true/false)"), longSleeveField,
+                new Label("Hood (JACKET true/false)"), hoodField,
                 new Label("Sole Type (SHOES)"), soleTypeField,
 
                 addBtn,
 
-                new Label("UUID"),
+                new Label("UUID Search"),
                 uuidField,
                 searchBtn,
 
